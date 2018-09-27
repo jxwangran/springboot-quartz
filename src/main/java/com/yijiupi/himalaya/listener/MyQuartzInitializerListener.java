fa4ff6f4bbdb2dbb6915abe42049219f76e3b930
wangran <wangran@yijiupi.com>
2018-09-27 09:13:45 +0800

    添加表

---
+++ src/main/java/com/yijiupi/himalaya/listener/MyQuartzInitializerListener.java
@@ -0,0 +1,18 @@
+/**   
+ * Copyright © 2018 北京易酒批电子商务有限公司. All rights reserved.
+ */
+package com.yijiupi.himalaya.listener;
+
+import org.quartz.ee.servlet.QuartzInitializerListener;
+
+/**    
+* @Title: MyQuartzInitializerListener.java  
+* @Package com.yijiupi.himalaya.listener  
+* @Description: 
+* @author wangran 
+* @date 2018年9月27日 上午9:09:40  
+* @version V1.0    
+*/
+public class MyQuartzInitializerListener extends QuartzInitializerListener {
+
+}
